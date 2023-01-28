f213c2482c6b23a3772687938f015e76c6533a2c
moazakhter99 <118044519+moazakhter99@users.noreply.github.com>
2023-01-29 02:04:56 +0530

    HelloGitHub

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package Basics;
+// Your First Program
+
+class HelloWorld { // Class Name
+
+    public static void main(String[] args) { // main method
+        System.out.println("Hello World!"); // printing Somethimg to user
+    }
+}
+
+// public static access specifiers
+// void not returning anything
+// main imp for running the program
+// String is the data type
+// args is custom can be given any name
+// main method comes under the scope of the class where the print statement is
